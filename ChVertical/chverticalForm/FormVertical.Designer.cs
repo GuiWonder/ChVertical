d752e84fa499d9b383bcc608895cbab39aaeb80a
GuiWonder <55447192+GuiWonder@users.noreply.github.com>
2023-03-06 21:12:52 +0800

    update

--- ChVertical/chverticalForm/FormVertical.Designer.cs
+++ ChVertical/chverticalForm/FormVertical.Designer.cs
@@ -43,6 +43,7 @@ namespace chvertical
             this.label2 = new System.Windows.Forms.Label();
             this.buttonFont = new System.Windows.Forms.Button();
             this.fontDialog = new System.Windows.Forms.FontDialog();
+            this.buttonFontFile = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownx)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDowny)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
@@ -57,15 +58,15 @@ namespace chvertical
             this.textBoxOut.Multiline = true;
             this.textBoxOut.Name = "textBoxOut";
             this.textBoxOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxOut.Size = new System.Drawing.Size(658, 260);
+            this.textBoxOut.Size = new System.Drawing.Size(666, 260);
             this.textBoxOut.TabIndex = 1;
             // 
             // buttonConvert
             // 
             this.buttonConvert.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonConvert.Location = new System.Drawing.Point(465, 3);
+            this.buttonConvert.Location = new System.Drawing.Point(417, 3);
             this.buttonConvert.Name = "buttonConvert";
-            this.buttonConvert.Size = new System.Drawing.Size(53, 24);
+            this.buttonConvert.Size = new System.Drawing.Size(47, 24);
             this.buttonConvert.TabIndex = 2;
             this.buttonConvert.Text = "开始";
             this.buttonConvert.UseVisualStyleBackColor = true;
@@ -74,7 +75,7 @@ namespace chvertical
             // numericUpDownx
             // 
             this.numericUpDownx.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.numericUpDownx.Location = new System.Drawing.Point(69, 4);
+            this.numericUpDownx.Location = new System.Drawing.Point(62, 4);
             this.numericUpDownx.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -86,7 +87,7 @@ namespace chvertical
             0,
             0});
             this.numericUpDownx.Name = "numericUpDownx";
-            this.numericUpDownx.Size = new System.Drawing.Size(60, 21);
+            this.numericUpDownx.Size = new System.Drawing.Size(53, 21);
             this.numericUpDownx.TabIndex = 3;
             this.numericUpDownx.Value = new decimal(new int[] {
             10,
@@ -97,7 +98,7 @@ namespace chvertical
             // numericUpDowny
             // 
             this.numericUpDowny.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.numericUpDowny.Location = new System.Drawing.Point(201, 4);
+            this.numericUpDowny.Location = new System.Drawing.Point(180, 4);
             this.numericUpDowny.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -109,7 +110,7 @@ namespace chvertical
             0,
             0});
             this.numericUpDowny.Name = "numericUpDowny";
-            this.numericUpDowny.Size = new System.Drawing.Size(60, 21);
+            this.numericUpDowny.Size = new System.Drawing.Size(53, 21);
             this.numericUpDowny.TabIndex = 4;
             this.numericUpDowny.Value = new decimal(new int[] {
             10,
@@ -123,31 +124,37 @@ namespace chvertical
             this.comboBoxTC.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxTC.FormattingEnabled = true;
             this.comboBoxTC.Items.AddRange(new object[] {
-            "粗实线+细实",
-            "双实线+细实",
-            "细实线+细虚",
-            "蝌蚪往回游",
-            "一切向上看",
-            "清风竹节型",
-            "密码错乱型",
-            "红星闪闪型",
-            "霓虹灯闪烁",
-            "我心依旧型",
-            "大圈套小圈",
-            "鱼儿水中游",
-            "阿米佗佛型",
-            "清爽空白型"});
-            this.comboBoxTC.Location = new System.Drawing.Point(267, 5);
+            "粗实线与细实",
+            "双实线与细实",
+            "双实线与细虚",
+            "细实线",
+            "细实线与细虚",
+            "向下箭头",
+            "向上箭头",
+            "竹节型",
+            "错乱型",
+            "五角星",
+            "霓虹灯",
+            "首尾三角",
+            "大小圆圈",
+            "首尾波浪线",
+            "卐字边",
+            "空白填充",
+            "上下空白",
+            "竖线格子",
+            "无线框",
+            "完全空白"});
+            this.comboBoxTC.Location = new System.Drawing.Point(239, 5);
             this.comboBoxTC.Name = "comboBoxTC";
-            this.comboBoxTC.Size = new System.Drawing.Size(106, 20);
+            this.comboBoxTC.Size = new System.Drawing.Size(95, 20);
             this.comboBoxTC.TabIndex = 5;
             // 
             // buttonCopy
             // 
             this.buttonCopy.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonCopy.Location = new System.Drawing.Point(524, 3);
+            this.buttonCopy.Location = new System.Drawing.Point(470, 3);
             this.buttonCopy.Name = "buttonCopy";
-            this.buttonCopy.Size = new System.Drawing.Size(73, 24);
+            this.buttonCopy.Size = new System.Drawing.Size(65, 24);
             this.buttonCopy.TabIndex = 6;
             this.buttonCopy.Text = "拷贝结果";
             this.buttonCopy.UseVisualStyleBackColor = true;
@@ -157,9 +164,9 @@ namespace chvertical
             // 
             this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(379, 7);
+            this.checkBox1.Location = new System.Drawing.Point(340, 7);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 16);
+            this.checkBox1.Size = new System.Drawing.Size(71, 16);
             this.checkBox1.TabIndex = 7;
             this.checkBox1.Text = "转换为繁体";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -178,7 +185,7 @@ namespace chvertical
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(664, 561);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(672, 561);
             this.tableLayoutPanel1.TabIndex = 8;
             // 
             // textBoxIn
@@ -189,21 +196,22 @@ namespace chvertical
             this.textBoxIn.Multiline = true;
             this.textBoxIn.Name = "textBoxIn";
             this.textBoxIn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxIn.Size = new System.Drawing.Size(658, 259);
+            this.textBoxIn.Size = new System.Drawing.Size(666, 259);
             this.textBoxIn.TabIndex = 2;
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 9;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tableLayoutPanel2.ColumnCount = 10;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.928572F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.928572F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.928572F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.928572F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.17857F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.60714F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.035714F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.71429F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.035714F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.71429F));
             this.tableLayoutPanel2.Controls.Add(this.numericUpDownx, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.buttonConvert, 6, 0);
             this.tableLayoutPanel2.Controls.Add(this.buttonCopy, 7, 0);
@@ -213,20 +221,21 @@ namespace chvertical
             this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.label2, 2, 0);
             this.tableLayoutPanel2.Controls.Add(this.buttonFont, 8, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonFontFile, 9, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 265);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(664, 30);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(672, 30);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Location = new System.Drawing.Point(3, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 8;
@@ -236,7 +245,7 @@ namespace chvertical
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(138, 9);
+            this.label2.Location = new System.Drawing.Point(121, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 12);
             this.label2.TabIndex = 8;
@@ -245,19 +254,30 @@ namespace chvertical
             // buttonFont
             // 
             this.buttonFont.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonFont.Location = new System.Drawing.Point(603, 3);
+            this.buttonFont.Location = new System.Drawing.Point(541, 3);
             this.buttonFont.Name = "buttonFont";
-            this.buttonFont.Size = new System.Drawing.Size(58, 24);
+            this.buttonFont.Size = new System.Drawing.Size(47, 24);
             this.buttonFont.TabIndex = 9;
             this.buttonFont.Text = "字体";
             this.buttonFont.UseVisualStyleBackColor = true;
             this.buttonFont.Click += new System.EventHandler(this.buttonFont_Click);
             // 
+            // buttonFontFile
+            // 
+            this.buttonFontFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonFontFile.Location = new System.Drawing.Point(594, 3);
+            this.buttonFontFile.Name = "buttonFontFile";
+            this.buttonFontFile.Size = new System.Drawing.Size(75, 24);
+            this.buttonFontFile.TabIndex = 10;
+            this.buttonFontFile.Text = "字体文件";
+            this.buttonFontFile.UseVisualStyleBackColor = true;
+            this.buttonFontFile.Click += new System.EventHandler(this.buttonFontFile_Click);
+            // 
             // FormVertical
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 561);
+            this.ClientSize = new System.Drawing.Size(672, 561);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FormVertical";
             this.Text = "文字竖排转换器";
@@ -286,6 +306,7 @@ namespace chvertical
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonFont;
         private System.Windows.Forms.FontDialog fontDialog;
+        private System.Windows.Forms.Button buttonFontFile;
     }
 }
 
